7936605f6fc63e1de09fd339f79822b50ff09e77
森邦彦 <WhiteBear-34@Whitebear50.local>
2017-06-29 19:44:31 +0900

    add t-test

--- platform.cpp
+++ platform.cpp
@@ -28,9 +28,23 @@ float calc_SD(vector<float> list)
     return sqrt(sum/list.size());
 }
 
-int main()
+float t_test(vector<float> list1, vector<float> list2)
 {
-    ifstream ifs("weights.txt");
+    float avr1 = calc_average(list1);
+    float avr2 = calc_average(list2);
+    float SD1  = calc_SD(list1);
+    float SD2  = calc_SD(list2);
+    float n1   = list1.size();
+    float n2   = list2.size();
+
+    float poolV = ( (n1-1)*pow(SD1,2) + (n2-1)*pow(SD2,2) ) / (n1+n2-2); 
+    float t = (avr1 - avr2) / sqrt(poolV * (1/n1 + 1/n2));
+    return t;
+}
+
+int main(int argc, char *argv[])
+{
+    ifstream ifs(argv[1]);
     if (ifs.fail()) {
         cerr << "fail" << endl;
         return -1;
@@ -66,7 +80,20 @@ int main()
         float average = calc_average(array);
         float SD      = calc_SD(array);
 
-        cout<< label << "\t" << average << "\t" << SD << endl;
+        cout<< label << "\t" << average << "\t\t" << SD << endl;
+    }
+    cout << endl;
+
+    if(table[0].size()==2){
+        //export 2column
+        vector<float> array1;
+        vector<float> array2;
+        for(int i=1; i<table.size(); i++){
+            array1.push_back(stof(table[i][0]));
+            array2.push_back(stof(table[i][1]));
+        }
+        cout<< "t = " << "\t" << t_test(array1,array2) << "\t" << endl;
+        cout << endl;
     }
     return 0;
 }
